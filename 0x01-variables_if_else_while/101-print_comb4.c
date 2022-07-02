e11a9f648bfb0feaa885e938c953575c109fdf19
Damo0007 <Bamigboyedamola74@gmail.com>
2022-07-02 12:28:59 -0700

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,15 +11,15 @@ int main(void)
 {
 
 int n, m, l;
-for (n = 45; n < 55; n++)
+for (n = 48; n < 58; n++)
 
 {
 
-for (m = 46; m < 55; m++)
+for (m = 49; m < 58; m++)
 
 {
 
-for (l = 47; l < 55; l++)
+for (l = 50; l < 58; l++)
 {
 
 if (l > m && m > n)
@@ -29,7 +29,7 @@ putchar(n);
 putchar(m);
 putchar(l);
 
-if (n != 50 || m != 51)
+if (n != 55 || m != 56)
 {
 putchar(',');
 
