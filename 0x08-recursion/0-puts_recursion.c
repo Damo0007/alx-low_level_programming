b58724cfbf37486754035eb3a23ca8bd5ddc14b2
Damo0007 <Bamigboyedamola74@gmail.com>
2022-07-20 12:45:23 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		_putchar(' ');
+		_putchar('\n');
 			return;
 	}
 	if (*s > 0)
 	{
 		_putchar(*s);
-		return;
-	}
 		_puts_recursion(s + 1);
+	}
 }
