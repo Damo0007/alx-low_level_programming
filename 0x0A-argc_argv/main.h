846954a10bd5f58ec2ede0684ab26a547e496eb4
Damo0007 <Bamigboyedamola74@gmail.com>
2022-07-22 12:53:34 -0700

    sub

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
+
+void _puts(char *);
+
+void print_number(int);
+
+int _atoi(char *);
+
+int isnumber(char *);
+
+int subtract_largest_coin(int);
+
 
 #endif
